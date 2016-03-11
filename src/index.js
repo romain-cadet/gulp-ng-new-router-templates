c4c7fb6d513d1cbad8bff30b1f9f5974debeaa89
romain-cadet <romain.cadet@gfi.fr>
2016-03-11 11:13:18 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,77 +1,86 @@
-'use strict'
-import _ from 'lodash'
-import {EOL} from 'os'
-import gutil from 'gulp-util'
-import path from 'path'
-import pkg from '../package.json'
-import through from 'through2'
+'use strict';
+
+var _ = require('lodash'),
+os = require('os'),
+gutil = require('gulp-util'),
+path = require('path'),
+pkg = 'rhweb',
+through = require('through2');
+
+function camelCase(str) {
+	return str.replace(/(-[A-Za-z]{1})/g, function($1) {
+		return $1.replace('-', '').toUpperCase();
+	});
+}
 
 module.exports = function (options) {
-  const files = []
-  let content = ''
+  const files = [];
+  let content = '';
 
   const defaults = {
     extension: '.tpl.html',
     fileName: 'templates.js',
     moduleName: 'app'
-  }
+  };
 
-  const opts = _.merge(defaults, options)
+  const opts = _.merge(defaults, options);
 
   const header = [
-    `(function () {${EOL}`,
-    `  angular${EOL}`,
-    `    .module('<%= moduleName %>')${EOL}`,
-    `    .config(['$componentLoaderProvider', function ($componentLoaderProvider) {${EOL}`
-  ].join('')
+    `(function () {`+os.EOL,
+    `  'use strict';`+os.EOL,
+    `  angular`+os.EOL,
+    `    .module('<%= moduleName %>')`+os.EOL,
+    `    .config(['$componentLoaderProvider', function ($componentLoaderProvider) {`+os.EOL
+  ].join('');
 
   const footer = [
-    `    }])${EOL}`,
-    '}())'
-  ].join('')
+    `    }]);`+os.EOL,
+    '}());'
+  ].join('');
 
   return through.obj((file, encoding, next) => {
-    if (!file || file.isNull()) {
-      return next()
+    if (!file || file.isNull() || file.path.indexOf('nrh-readme') >= 0 || file.path.indexOf('panels') >= 0) {
+      return next();
     }
 
     if (file.isStream()) {
-      return this.emit('error', new gutil.PluginError(pkg.name, 'Streaming not supported'))
+      return this.emit('error', new gutil.PluginError(pkg, 'Streaming not supported'));
     }
 
-    files.push(file)
+    files.push(file);
 
-    next()
+    next();
   }, function (callback) {
     if (files.length > 0) {
       content = files.map(file => {
-        let component
+        let component;
         // 'component-name':
-        component = `          '${path.basename(file.path).replace(opts.extension, '')}': `
+        component = `          '${path.basename(file.path).replace(opts.extension, '')}': `;
+        component = camelCase(component);
         // 'component-name': 'relative/path/to/component-name.html'
-        component += `'${path.relative(file.base, file.path).replace(/\\/g, '/')}'`
-        return component
-      }).join(`,${EOL}`)
+        component += `'${path.relative(file.base, file.path).replace(/\\/g, '/').replace("../src/main/webapp", ".")}'`;
+        return component;
+      }).join(`,`+os.EOL);
       // $componentLoaderProvider.setTemplateMapping(function (name) {
       //   return {
       //     'component-name': 'relative/path/to/component-name.html'
       //   }[name]
       // })
       content = [
-        `      $componentLoaderProvider.setTemplateMapping(function (name) {${EOL}`,
-        `        return {${EOL}`
-      ].join('') + content + EOL
-      content += [`        }[name]${EOL}`,
-                 `      })${EOL}`].join('')
+        `      $componentLoaderProvider.setTemplateMapping(function (name) {`+os.EOL,
+        `        return {`+os.EOL
+      ].join('') + content + os.EOL;
+      content += [`        }[name];`+os.EOL,
+                 `      });`+os.EOL].join('');
     }
 
-    const templates = _.template(header + content + footer)(opts)
+    const templates = _.template(header + content + footer)(opts);
     /* eslint no-invalid-this: 0 */
     this.push(new gutil.File({
       base: '',
       path: opts.fileName,
       contents: new Buffer(templates)
-    }))
-    callback()
-  })
-}
+    }));
+    callback();
+  });
+};
